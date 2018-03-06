dde9ed6b99915e666d2fcaf87277f6574343d7d6
PDunipace22 <peterdunipace@gmail.com>
2018-03-06 09:48:59 -0500

    Initial Commit

---
+++ BankAccountMVC2/AccountModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace BankAccountMVC2
+{
+    public class AccountModel
+    {
+        [Key]
+        public int AccountID { get; set; }
+
+        public string First { get; set; }
+        public string Last{get;set;}
+        public int Month { get; set; }
+        public int Year { get; set; }
+    }
+}
---
+++ BankAccountMVC2/Models/AccountModelsController.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using BankAccountMVC2;
+
+namespace BankAccountMVC2.Models
+{
+    public class AccountModelsController : Controller
+    {
+        private BankAccountMVC2Context db = new BankAccountMVC2Context();
+
+        // GET: AccountModels
+        public ActionResult Index()
+        {
+            return View(db.AccountModels.ToList());
+        }
+
+        // GET: AccountModels/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            AccountModel accountModel = db.AccountModels.Find(id);
+            if (accountModel == null)
+            {
+                return HttpNotFound();
+            }
+            return View(accountModel);
+        }
+
+        // GET: AccountModels/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: AccountModels/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "AccountID,First,Last,Month,Year")] AccountModel accountModel)
+        {
+            if (ModelState.IsValid)
+            {
+                db.AccountModels.Add(accountModel);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(accountModel);
+        }
+
+        // GET: AccountModels/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            AccountModel accountModel = db.AccountModels.Find(id);
+            if (accountModel == null)
+            {
+                return HttpNotFound();
+            }
+            return View(accountModel);
+        }
+
+        // POST: AccountModels/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "AccountID,First,Last,Month,Year")] AccountModel accountModel)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(accountModel).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(accountModel);
+        }
+
+        // GET: AccountModels/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            AccountModel accountModel = db.AccountModels.Find(id);
+            if (accountModel == null)
+            {
+                return HttpNotFound();
+            }
+            return View(accountModel);
+        }
+
+        // POST: AccountModels/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            AccountModel accountModel = db.AccountModels.Find(id);
+            db.AccountModels.Remove(accountModel);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
